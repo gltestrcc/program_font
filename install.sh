04bef4530b607dc58e9a831ac5ee382571bc798d
gltestrcc <36440624+gltestrcc@users.noreply.github.com>
2018-02-13 12:18:14 +0200

    Update install.sh
    excluded sudo which was causing error during docker image building

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ else
   # Linux
   font_dir="${HOME}/.fonts/"
   mkdir -p $font_dir
-  sudo mkdir -p /usr/share/fonts/MyFonts
+  mkdir -p /usr/share/fonts/MyFonts
   find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec sudo cp {} /usr/share/fonts/MyFonts  \;
 fi
 
 echo "Install start..."
 find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec cp {} ${font_dir}  \;
-sudo fc-cache -f -v
+fc-cache -f -v
 echo "Install finish..."
